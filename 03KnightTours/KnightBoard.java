1ce1c0fb5eb57c54fdf9de415b75397af11da76c
Steve Wong <swong14@stuy.edu>
2017-02-15 12:33:40 -0500

    worked on knightboard.java

--- 03KnightTours/KnightBoard.java
+++ 03KnightTours/KnightBoard.java
@@ -1,1 +1,34 @@
+public class KnightBoard() {
+    public KnightBoard(int startingRows,int startingCols) {
+	board = new int[startingRow][startingCols];
+    }
 
+    public String toString() {
+	String kboard = "";
+	for (int r = 0; r < board.length; r++) {
+	    for(int c = 0; c < board[0].length; c++) {
+		
+		/*
+		if (board[r][c] != -1) {
+		    kboard = kboard + " " +  "_";
+		}
+		else {
+		    kboard= kboard + " " + "Q";
+		}
+		*/
+	    }
+	    kboard += "\n";
+	}
+	return kboard;
+    }
+
+    public void solve() {
+
+    }
+
+    private boolean solveH(int row ,int col, int level) {
+
+    }
+
+
+}
