8a22c4018f51fc5706f0946de98c656ac1936876
Steve Wong <swong14@stuy.edu>
2017-02-16 12:33:17 -0500

    started working on my isGoodSpot

--- 03KnightTours/KnightBoard.java
+++ 03KnightTours/KnightBoard.java
@@ -1,21 +1,23 @@
-public class KnightBoard() {
+public class KnightBoard{
+    private int[][]board;
     public KnightBoard(int startingRows,int startingCols) {
-	board = new int[startingRow][startingCols];
+	board = new int[startingRows][startingCols];
     }
 
     public String toString() {
 	String kboard = "";
 	for (int r = 0; r < board.length; r++) {
 	    for(int c = 0; c < board[0].length; c++) {
-		
+		kboard = kboard + " " + board[r][c];
 		/*
 		if (board[r][c] != -1) {
-		    kboard = kboard + " " +  "_";
+		    kboard = kboard + " " +  0;
 		}
 		else {
-		    kboard= kboard + " " + "Q";
+		    kboard= kboard + " " + 1;
 		}
 		*/
+		
 	    }
 	    kboard += "\n";
 	}
@@ -23,11 +25,31 @@ public class KnightBoard() {
     }
 
     public void solve() {
-
+	solveH(0,0,1);
     }
 
     private boolean solveH(int row ,int col, int level) {
+	return true;
+    }
+    public boolean isGoodSpot(int row, int col) {
+	if (row < board.length   &&
+	    row >= 0             &&
+	    col < board[0].length&&
+	    col >= 0             &&
+	    board[row][col] == 0) {
+	    return true;
+	}
+	else {
+	    return false;
+	}
 
+    }
+    public static void main(String args[]){
+	KnightBoard a = new KnightBoard(8,8);
+	System.out.println(a);
+	System.out.println(a.isGoodSpot(0,0));
+	System.out.println(a.isGoodSpot(8,8));
+	System.out.println(a.isGoodSpot(7,8));
     }
 
 
