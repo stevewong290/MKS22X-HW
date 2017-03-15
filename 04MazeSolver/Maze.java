1e73418664b72f5f11fc416a24b55e67c3603c4e
Steve Wong <swong14@stuy.edu>
2017-03-15 02:26:06 -0400

    ah yes, fixed the maze

--- 04MazeSolver/Maze.java
+++ 04MazeSolver/Maze.java
@@ -54,42 +54,51 @@ public class Maze{
     }
     */
  public Maze(String filename, boolean b){
-	animate = b;
-        String ans = "";
-	try{
-	    int numE = 0;
-	    int numS = 0;
-	    File text = new File(filename);
-	    Scanner infile1 = new Scanner(text);
-	    int r = 0;
-	    int c = 0;
-	    while(infile1.hasNextLine()){
-		String line = infile1.nextLine();
-            ans += line;
-		c = line.length();
-		r += 1;
-	    }
-        maze = new char[r][c];
-        while (ans.length() > 0) {
-            for(int i = 0; i < maze.length; i++) {
-                for(int j = 0; j < maze[0].length; j++) {
-                    maze[i][j] = ans.charAt(0);
-                    ans = ans.substring(1);
-                }
-            }
-        }
-	    if(numS != 1 || numE != 1) {
-		System.out.println("you either have too many or no E's or S's");
-		System.exit(1);
-	    }
-	}
-	catch(FileNotFoundException e) {
-	    System.out.println("Missing File");
-	    System.exit(0);
-	}
-    }
-    public void setAnimate(boolean b){
+     animate = b;
+     String ans = "";
+     try{
+	 int numE = 0;
+	 int numS = 0;
+	 File text = new File(filename);
+	 Scanner infile1 = new Scanner(text);
+	 int r = 0;
+	 int c = 0;
+	 while(infile1.hasNextLine()){
+	     String line = infile1.nextLine();
+	     ans += line;
+	     c = line.length();
+	     r += 1;
+	 }
+	 maze = new char[r][c];
+	 while (ans.length() > 0) {
+	     for(int i = 0; i < maze.length; i++) {
+		 for(int j = 0; j < maze[0].length; j++) {
+		     maze[i][j] = ans.charAt(0);
+		     if(maze[i][j] == 'E'){
+			 numE++;
+		     }
+		     if(maze[i][j] == 'S'){
+			 numS++;
+			 SrHolder = i;
+			 ScHolder = j;
+		     }
 
+		     ans = ans.substring(1);
+		 }
+	     }
+	 }
+	 if(numS != 1 || numE != 1) {
+	     System.out.println("you either have too many or no E's or S's");
+	     System.exit(1);
+	 }
+     }
+     catch(FileNotFoundException e) {
+	 System.out.println("Missing File");
+	 System.exit(0);
+     }
+ }
+    public void setAnimate(boolean b){
+	
         animate = b;
 
     }
@@ -131,40 +140,62 @@ public class Maze{
 	    System.out.println("\033[2J\033[1;1H"+ toString());
             wait(100);
 	}
-	if(maze[r][c] == 'E'){
-	    return true;
-	}
-	maze[r][c] = '&';
-	if(maze[r + 1][c] == ' ' || maze[r + 1][c] == 'E'){
-	    if(solve(r + 1, c)){
+	if (r < maze.length && r >= 0 && c < maze[0].length && c >= 0) {
+
+	    if(maze[r][c] == 'E'){
 		return true;
 	    }
-	}
-	if(maze[r - 1][c] == ' ' || maze[r - 1][c] == 'E'){
-	    if(solve(r - 1, c)){
-		return true;
+	    //	maze[r][c] = '&';
+	    if (maze[r][c] == ' '){
+		maze[r][c] = '&';
+		if(solve(r + 1, c)){
+		    return true;
+		}
+		if(solve(r - 1, c)){
+		    return true;
+		}
+		if(solve(r, c + 1)){
+		    return true;
+		}
+		if(solve(r, c - 1)){
+		    return true;
+		}
+		else{
+		    maze[r][c] = '.';
+		}
+		return false;
+
 	    }
-	}
-	if(maze[r][c + 1] == ' ' || maze[r][c + 1] == 'E'){
-	    if(solve(r, c + 1)){
+	    
+	    /*	if(maze[r + 1][c] == ' ' || maze[r + 1][c] == 'E'){
+		if(solve(r + 1, c)){
 		return true;
-	    }
-	}
-	if(maze[r][c - 1] == ' ' || maze[r][c - 1] == 'E'){
-	    if(solve(r, c - 1)){
+		}
+		if(maze[r - 1][c] == ' ' || maze[r - 1][c] == 'E'){
+		if(solve(r - 1, c)){
 		return true;
-	    }
+		}
+		}
+		if(maze[r][c + 1] == ' ' || maze[r][c + 1] == 'E'){
+		if(solve(r, c + 1)){
+		return true;
+		}
+		}
+		if(maze[r][c - 1] == ' ' || maze[r][c - 1] == 'E'){
+		if(solve(r, c - 1)){
+		return true;
+		}
+		}*/
+
+	    return false;
 	}
-	maze[r][c] = '.';
 	return false;
-		
     }      
     
     public static void main(String[] args){
-	Maze a = new Maze("data2.dat", true);
+	Maze a = new Maze("data3.dat", true);
 	System.out.println(a);
 	a.solve();
-	System.out.println(a);
     }
    
 }
