920cf1cd6b7d6434755757e5e9968479725d0844
Steve Wong <stevewong@Steves-iMac.home>
2017-04-02 01:16:15 -0400

    oh my lord jesus have mercy on my soul. turcotti saved me with the nice debugging on merge.java. it was nice because we spent 5 minutes looking at incorrect parentheses

--- 08Merge/Merge.java
+++ 08Merge/Merge.java
@@ -1,5 +1,8 @@
-public class Merge(){
+import java.util.*;
+
+public class Merge{
     public static void mergesort(int[] array){
+	System.out.println(array.length);
 	int[] left  = new int[array.length / 2];
 	int[] right = new int[array.length - array.length / 2];
 	    
@@ -11,10 +14,12 @@ public class Merge(){
 	    int ltemp = 0;
 	    while ( ltemp  < array.length / 2 ) {
 		left[ltemp] = array[ltemp];
+		ltemp++;
 	    }
 	    int rtemp = 0;
 	    while ( rtemp < array.length - array.length / 2 ) {
 		right[rtemp] = array[rtemp + array.length / 2];
+		rtemp++;
 	    }
 	    mergesort(left);
 	    mergesort(right);
@@ -23,7 +28,31 @@ public class Merge(){
     }
 
     public static void merge(int[]a, int[]b, int[]destination){
+	int i = 0;
+	int j = 0;
+	for (int x = 0; x < destination.length; x++) {
+	    
+	    if(i <  a.length && (j >= b.length || a[i] <  b[j])) {
+		destination[x] = a[i];
+		i++;
+	    }
+	    else {
+		destination[x] = b[j];
+		j++;
+	    }
+	}
+    }
 
+    public static void main(String[] args){
+	int[] hello = new int[]{1, 7, 10};
+	int[] bitch = new int[]{4, 5, 6};
+	int[] destibitchation = new int[6];
+	merge(hello, bitch, destibitchation);
+	System.out.println(Arrays.toString(destibitchation));
+	
+	int[] turcotti = new int[]{8,5,6,7,2,0,2,8,1,6,3,1,4,8,2,4,1,6,4,6,5,8,0,1,3,5,7,2};
+	mergesort(turcotti);
+	System.out.println(Arrays.toString(turcotti)); 
     }
 
 }
