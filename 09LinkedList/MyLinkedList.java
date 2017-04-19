91eaa567aa4d3cc380f1aada0766d23bd622f5da
Steve Wong <swong14@stuy.edu>
2017-04-19 12:25:31 -0400

    heyo

--- 09LinkedList/MyLinkedList.java
+++ 09LinkedList/MyLinkedList.java
@@ -169,7 +169,7 @@ public class MyLinkedList implements Iterable<Integer>{
 	MyLinkedList mll = new MyLinkedList();
 	for (int i=0; i<10; i++)
 	    mll.add(i);
-	/*	System.out.println(mll);
+		System.out.println(mll);
 	mll.add(5, 10);
 	mll.add(mll.size(), 10);
 	System.out.println(mll);
@@ -181,7 +181,7 @@ public class MyLinkedList implements Iterable<Integer>{
 	System.out.println(mll);
 	System.out.println(mll.indexOf(10));
 	System.out.println(mll.indexOf(11));
-*/
+
 	for(int x: mll){
 	    System.out.println(x);
 	}
