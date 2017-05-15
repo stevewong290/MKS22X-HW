6218e2409279735c0b2eb05be206e300314143ff
Steve Wong <swong14@stuy.edu>
2017-05-15 12:35:54 -0400

    getting it together

---
+++ 15TheBetterMazeSolver/Location.java
@@ -0,0 +1,26 @@
+public class Location implements Comparable<Location>{
+    private int distToGoal;
+    private boolean aStar;
+
+    public Location(int r, int c,
+		    Location previous,
+		    int distToStart,
+		    int distToGoal){
+
+    }
+    public Location(int r, int c,
+		    Location previous,
+		    int distToStart,
+		    int distToGoal,
+		    boolean aStar){
+	
+    }
+
+    public int CompareTo(Location other){
+
+    }
+
+    public int distTGaccess(){
+
+    }
+}
